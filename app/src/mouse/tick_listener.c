1c9c5448b184136110765f2210bd7c866a438c20
pyg <keeeen@gmail.com>
2023-01-22 16:49:41 -0800

    mouse time fraction hack

--- app/src/mouse/tick_listener.c
+++ app/src/mouse/tick_listener.c
@@ -50,6 +50,9 @@ static float speed(const struct mouse_config *config, float max_speed, int64_t d
         return max_speed;
     }
     float time_fraction = (float)duration_ms / config->time_to_max_speed_ms;
+    if (time_fraction < 0.2) {
+        time_fraction = 0.2;
+    }
     return max_speed * powf(time_fraction, config->acceleration_exponent);
 }
 
